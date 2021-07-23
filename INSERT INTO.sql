7d13b79b52f3555f6404b204c8c70a34431763e2
leandrosantos2018 <40071307+leandrosantos2018@users.noreply.github.com>
2021-07-23 05:52:08 -0300

    up

---
+++ INSERT INTO.sql
@@ -0,0 +1,11 @@
+SELECT * FROM test.estados;
+
+INSERT INTO estados (CODIGO,NOME) values(4,'P');
+INSERT INTO estados (CODIGO,NOME) values(3,'BAHIA');
+INSERT INTO estados (CODIGO,NOME) values(3,'BAHIA');
+INSERT INTO estados (CODIGO,NOME) values(3,'BAHIA');
+INSERT INTO estados (CODIGO,NOME) values(3,'BAHIA');
+
+ 
+
+
